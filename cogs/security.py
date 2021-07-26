789a7b9dccccf891609287d04e20cfb1ea8dafc1
IsaiahASmith <isaiah@clsolutions.ltd>
2021-07-25 22:16:52 -0400

    Add get hole info

--- cogs/security.py
+++ cogs/security.py
@@ -18,16 +18,20 @@ class Security(Cog):
         self.bot = bot
         self._pending_holes = {}
 
-    async def open_hole(self, members: Set[Member], password: str) -> int:
+    async def open_hole(self, members: Set[Member], password: str, *args) -> int:
         """Provides an id to check against"""
         cur_id = self._id_count
-        self._pending_holes.update({cur_id: (members, password)})
+        self._pending_holes.update({cur_id: (members, password, *args)})
         self._id_count += 1
         return cur_id
 
     async def close_hole(self, hole_id: int):
         """Removes a hole the check against"""
         del self._pending_holes[hole_id]
+
+    async def get_hole_info(self, hole_id: int, member: Member, password: str):
+        if await self.check_hole(hole_id, member, password):
+            return list(self._pending_holes[hole_id])[2:]  # Remove the members and password from the hole
 
     async def check_hole(self, hole_id: int, member: Member, password: str) -> bool:
         """Allows processing through a hole with a valid hole, member, and password"""
