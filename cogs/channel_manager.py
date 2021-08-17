eceed7a345f2425ce58c71d6153a002f8c408f26
IsaiahASmith <isaiah@clsolutions.ltd>
2021-08-17 16:39:39 -0400

    Remove security related commands

--- cogs/channel_manager.py
+++ cogs/channel_manager.py
@@ -76,55 +76,6 @@ class ChannelManager(Cog):
         session.commit()
         await ctx.send(embed=embed)
 
-    @command(name="register_channel_from_token", aliases=["rcft"])
-    @has_permissions(manage_guild=True)
-    async def register_channel_from_token(self, ctx, channel: ChannelConverter, group_token: int, password: str):
-        security_cog: Security = self.bot.cogs_lookup["security"]
-        info = await security_cog.get_hole_info(group_token, ctx.author, password)
-        if info is None:
-            await ctx.send("Permission Denied")
-            return
-        group = info[0]
-        await security_cog.close_hole(group_token)  # Close the hole as it is useless now
-        session.add(ChannelGroupChannel(channel_group_id=group.id, channel_id=channel.id))
-        session.commit()
-        embed = group_to_embed(self.bot, group, "Channels Registered", ctx.author.colour)
-        await ctx.send(embed=embed)
-
-    @command(name="generate_access_token", aliases=["gat"])
-    @has_permissions(manage_guild=True)
-    async def generate_access_token(
-        self, ctx, group: ChannelGroupConverter, members: Greedy[Member], duration: Optional[int] = 60.0
-    ):
-        members = {member for member in members}
-
-        token = None
-        dm_channel = None
-
-        def check(message) -> bool:
-            if isinstance(message.channel, DMChannel):
-                nonlocal token
-                nonlocal dm_channel
-                token = message.content
-                dm_channel = message.channel
-                return True
-            return False
-
-        try:
-            await ctx.channel.send("DM me your password")
-            await self.bot.wait_for("message", timeout=60.0, check=check)
-        except AsyncTimeoutError:
-            await ctx.channel.send("Timeout")
-            return None
-
-        await dm_channel.send(f"The password is: {token}")
-        security_cog: Security = self.bot.cogs_lookup["security"]
-        hole_id = await security_cog.open_hole(members, token, group)
-        await ctx.send(f"The group token is {hole_id!s}")
-
-        await sleep(duration)
-        await security_cog.close_hole(hole_id)
-
     @command(name="register_channel", aliases=["rc"])
     @has_permissions(manage_guild=True)
     async def register_channel(self, ctx, channel: ChannelConverter, group: ChannelGroupConverter):
